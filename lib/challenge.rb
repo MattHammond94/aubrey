dceb1f577a7fdfede1908d0464048c23d1b8eebe
Matt Hammond <mjhammond94@gmail.com>
2023-04-14 17:37:34 +0100

    removed fail and added conditional - spec tests updated accordingly

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -5,12 +5,25 @@ class GrammarStats
   end
 
   def check(text)
-    fail "Not a valid input" unless text.is_a?(String)
-    @total_count += 1
-    text.start_with?(/[A-Z]/) && text.end_with?("?", "!", ".") ? @true_count +=1 : nil
+    if text.is_a?(String)
+      @total_count += 1
+      text.start_with?(/[A-Z]/) && text.end_with?("?", "!", ".") ? @true_count += 1 : nil
+    else 
+      @total_count += 0 
+    end
   end
 
   def percentage_good
     ((@true_count.to_f / @total_count) * 100).round
   end
 end
+
+# Had a conversation with Leo around error catching to avoid program ending when a non String
+# Data type is passed to check. 
+
+# Can add a further method that only checks the data type and then catches an error and 
+# prompts user for another input if not a String. This method would then be called in our check method. 
+
+# Due to wanting our program to only have 3 methods as per the challenge task
+# I have added a conditional to check method arg is a String to avoid error ending program.
+# These tests have also been re-added.
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -12,13 +12,24 @@ describe GrammarStats do
     end
   end
 
-  context 'Fails for check' do
-    it 'Should return an error message for any checks on non string values' do
-      gram = GrammarStats.new
-      expect { gram.check(nil) }.to raise_error "Not a valid input"
-      expect { gram.check(:symbol) }.to raise_error "Not a valid input"
-      expect { gram.check([]) }.to raise_error "Not a valid input"
-      expect { gram.check(12) }.to raise_error "Not a valid input"
+  # context 'Fails for check' do
+  #   it 'Should return an error message for any checks on non string values' do
+  #     gram = GrammarStats.new
+  #     expect { gram.check(nil) }.to raise_error "Not a valid input"
+  #     expect { gram.check(:symbol) }.to raise_error "Not a valid input"
+  #     expect { gram.check([]) }.to raise_error "Not a valid input"
+  #     expect { gram.check(12) }.to raise_error "Not a valid input"
+  #   end
+  # end
+
+  context 'percentage ran after multiple checks called with non strings as arguments' do
+    it 'Should return 100 when checks failed' do
+      gram = GrammarStats.new 
+      gram.check(nil)
+      gram.check(12)
+      gram.check(:symbol)
+      gram.check("True.")
+      expect(gram.percentage_good).to eq 100
     end
   end
 
